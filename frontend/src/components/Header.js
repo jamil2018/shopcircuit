8f24f2019396aea55004ebb744c5dc305031c198
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-11 17:29:29 +0600

    changelog: 1. implement react-router 2. fix route links on Header component 2. fix route links on Product component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,18 +7,24 @@ const Header = () => {
     <header className="mb-4">
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Shop Circuit</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Shop Circuit</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart" className="mx-3 px-0">
-                <i className="fas fa-shopping-cart mr-1"></i>
-                CART
-              </Nav.Link>
-              <Nav.Link href="/login" className="mx-3 px-0">
-                <i className="fas fa-user mr-1"></i>
-                SIGN IN
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="mx-3 px-0">
+                  <i className="fas fa-shopping-cart mr-1"></i>
+                  CART
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="mx-3 px-0">
+                  <i className="fas fa-user mr-1"></i>
+                  SIGN IN
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProduceScreen = () => {
+  return <h1>All Products</h1>;
+};
+
+export default ProduceScreen;
