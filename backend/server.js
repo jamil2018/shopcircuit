a53751aef92b2f83f2bea69d82fee428ffc24c74
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-11 23:11:29 +0600

    changelog: 1. setup product fetching routes 2. add product data to backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import products from './data/products.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use('/', (req, res) => {
   res.send('hello world');
+});
+app.use('/api/products', (req, res) => {
+  res.json(products);
+});
+app.use('/api/products/:id', (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(PORT, () => {
