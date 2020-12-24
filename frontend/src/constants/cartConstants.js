7669a2bc2fa49f6461a42065343b89e1e5f62ab4
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-24 16:28:30 +0600

    changeset: 1. add cart actions 2. add cart constants 3. cart cart reducers 4. call action from cart screen

---
+++ frontend/src/constants/cartConstants.js
@@ -0,0 +1,2 @@
+export const CART_ADD_ITEM = 'CART_ADD_ITEM';
+export const REMOVE_ADD_ITEM = 'REMOVE_ADD_ITEM';
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,21 @@
-import React from 'react';
-const CartScreen = () => {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <h2>Cart</h2>;
 };
 
