5b6229bdb0e824aa1710a94d50aab7dadd79ae68
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-01-10 12:56:24 +0600

    changeset: 1. create updateProfile controller 2. create updateProfile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 /**
  * @desc Get user profile
- * @route GET /api/users/login
+ * @route GET /api/users/profile
  * @access private
  */
 const getUserProfile = asyncHandler(async (req, res) => {
@@ -83,4 +83,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+/**
+ * @desc Update user profile
+ * @route PUT /api/users/profile
+ * @access private
+ */
+const updateUserProfile = asyncHandler(async (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      // eslint-disable-next-line no-underscore-dangle
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      // eslint-disable-next-line no-underscore-dangle
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
