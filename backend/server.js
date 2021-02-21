369f5e353372591d0d5f320800d2e81ec5b1ea84
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-02-21 20:47:20 +0600

    changeset: 1. add order route and controller to backend 2. implement shipping screen on frontend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import colors from "colors";
 
 import productsRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use("/api/products", productsRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const App = () => {
   return (
@@ -19,7 +21,9 @@ const App = () => {
       <main>
         <Container>
           <Route path="/login" component={LoginScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
           <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
